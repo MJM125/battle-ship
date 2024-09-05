b58676491117188ff6897422da9486736f2abd6d
Matthew <mattjmears371@gmail.com>
2024-09-05 11:15:47 +0000

    Initial starting sequence

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print('Welcome to Battleships for 1 player. Please be careful with entires, as if you get it wrong, you will still lose a go')
+print('Good Luck')
+print('')
+no_of_goes = int(input("How many goes you would like?: "))
+size_of_board = int(input("How big would you like the board to be?: "))
+if size_of_board > 50:
+    print("The board will be too big to fit on the screen (max 50)")
+    size_of_board = int(input('Choose a more sensible number: '))
+no_of_ships = int(input("Finally, how many ships would you like?: "))
